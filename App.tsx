14b19aaf3963e0c72bb4e2e087f350c3f4dbe45d
Aleilson Gomes de Cerqueira <aleilson.cerqueira@compasso.com.br>
2023-05-26 09:47:53 -0300

    Added onesignal

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,9 +9,34 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
+
+OneSignal.setAppId("59c7f16b-f798-4e70-8038-60b21bb9dbb7");
+OneSignal.setEmail('aleilson@email.com');
+
+OneSignal.promptForPushNotificationsWithUserResponse();
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  tagUserInfoCreate();
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationOpenedHandler((response) => {
+      const { actionId } = response.action as any;
+
+      switch(actionId) {
+        case '1':
+          return console.log('Ver todas');
+        case '2':
+          return console.log('Ver pedido');
+        default:
+          return console.log('Não foi clicado em botão de ação');
+      }
+    });
+
+    return () => unsubscribe;
+  },[]);
 
   return (
     <NativeBaseProvider theme={THEME}>
